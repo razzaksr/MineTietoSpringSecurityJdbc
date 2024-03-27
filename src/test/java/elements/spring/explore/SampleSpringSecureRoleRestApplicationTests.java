c25c994c6c63f5a0ca97a647df7e064b8974ad73
razzaksr <razzaksr@gmail.com>
2024-03-28 01:24:05 +0530

    Spring Security test done

--- src/test/java/elements/spring/explore/SampleSpringSecureRoleRestApplicationTests.java
+++ src/test/java/elements/spring/explore/SampleSpringSecureRoleRestApplicationTests.java
@@ -1,13 +1,97 @@
 package elements.spring.explore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SampleSpringSecureRoleRestApplicationTests {
 
+	@MockBean
+	private MyBankService myBankService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	@WithMockUser(username="testuser", authorities={"manager"})
+	public void testManagerAccess() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/credit/view"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+//	failure testcase as per expectation
+//	@Test
+//	@WithMockUser(username="testuser", authorities={"admin"})
+//	public void testManagerAccess() throws Exception {
+//		mockMvc.perform(MockMvcRequestBuilders.get("/credit/view"))
+//				.andExpect(MockMvcResultMatchers.status().isOk());
+//	}
+
+	@Test
+	@WithMockUser(username="testuser", authorities={"viewer"})
+	public void testViewerAccess() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/credit/one/1"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	@WithMockUser(username="testuser", authorities={"admin"})
+	public void testAdminAccess() throws Exception {
+		String request="{\n" +
+				"    \"creditcardNumber\": 56787676767,\n" +
+				"    \"creditcardCvv\": 932,\n" +
+				"    \"creditcardExpiry\": \"2035-10-21\",\n" +
+				"    \"creditcardPin\": 984,\n" +
+				"    \"creditcardLimit\": 50000,\n" +
+				"    \"creditcardUsage\": 11500,\n" +
+				"    \"creditcardAvailable\": 38500,\n" +
+				"    \"creditcardStatus\": true,\n" +
+				"    \"creditcardHolder\": \"Medhini\"\n" +
+				"}";
+		mockMvc.perform(MockMvcRequestBuilders.put("/credit/swipe/2").contentType(MediaType.APPLICATION_JSON).content(request))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+//	failed as per our expectation
+//	@Test
+//	@WithMockUser(username="testuser", authorities={"admin"})
+//	public void testAdminAccess() throws Exception {
+//		mockMvc.perform(MockMvcRequestBuilders.put("/credit/one/1"))
+//				.andExpect(MockMvcResultMatchers.status().isOk());
+//	}
+
+	@Test
+	@WithMockUser(username="testuser", authorities={"admin"})
+	public void testAdminPostAccess() throws Exception {
+		String request="{\n" +
+				"    \"creditcardNumber\": 56787676767,\n" +
+				"    \"creditcardCvv\": 932,\n" +
+				"    \"creditcardExpiry\": \"2035-10-21\",\n" +
+				"    \"creditcardPin\": 984,\n" +
+				"    \"creditcardLimit\": 50000,\n" +
+				"    \"creditcardUsage\": 11500,\n" +
+				"    \"creditcardAvailable\": 38500,\n" +
+				"    \"creditcardStatus\": true,\n" +
+				"    \"creditcardHolder\": \"Medhini\"\n" +
+				"}";
+		mockMvc.perform(MockMvcRequestBuilders.post("/credit/new").contentType(MediaType.APPLICATION_JSON).content(request))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	@WithMockUser(username="testuser")
+	public void testUnauthorizedAccess() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/credit/view"))
+				.andExpect(MockMvcResultMatchers.status().isForbidden());
 	}
 
 }
